7e338705d82727f33a522c3d6e6187ec7253edd2
Ameen Reda <ameenreda53@gmail.com>
2020-11-25 01:14:57 +0200

    encryption&decryption

---
+++ draw1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	
+//	int i=0;
+//
+//	for(i;i<9;i++){
+//		int j=0;
+//		for(j;j<9;j++){
+//			if(i==0 ||i==8){
+//				if(j==3 ||j==4 ||j==5){
+//					printf("*");
+//				}
+//				else printf(" ");
+//			}
+//			else if(i==1 ||i==7){
+//				if(j==1 ||j==7){
+//					printf("*");
+//				}
+//					else printf(" ");
+//			}
+//			else{
+//					if(j==0 ||j==8){
+//					printf("*");
+//				}
+//					else printf(" ");
+//			}
+//		
+//		}
+//		printf("\n");
+//	}
+//	
+	printf("%s","lol");
+	return 0;
+}
---
+++ split2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+	print("%d",45454);
+	return 0;
+}
